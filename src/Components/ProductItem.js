2c81fe921c53112a161f257d437fc3e5bbe06519
Marvin <marvinthompson@pursuit.org>
2022-11-04 03:32:56 -0400

    Added modal to product page

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { stripHtml } from "string-strip-html";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 const productClassNames = {
   productName: "product__name",
@@ -12,7 +16,23 @@ const productClassNames = {
   productPrice: "product__price",
 };
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const ProductItem = ({ product, onAddToCart }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const { result } = stripHtml(product.description);
   const {
     productCard,
@@ -55,10 +75,76 @@ const ProductItem = ({ product, onAddToCart }) => {
         >
           Quick add
         </button>
-        <button name="View product" className="product__view">
+        <button
+          name="View product"
+          className="product__view"
+          onClick={handleOpen}
+        >
           View
         </button>
       </container>
+
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              <h4 className={productName}>{product.name}</h4>
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <container>
+                <container className={"product__modalImageContainer"}>
+                  <img
+                    className="product__modalImage"
+                    src={product.image?.url}
+                    alt={product.name}
+                    style={{
+                      width: "100%",
+                      marginBottom: "0.5rem"
+                    }}
+                  />
+                </container>
+                <div>
+                  <div className={productInfo}>
+                    <p className={productDescription}>
+                      {/* product description stripped of html tags */}
+                      {result}
+                    </p>
+                    <div className={productDetails}>
+                      <p className={productPrice}>
+                        {product.price.formatted_with_symbol}
+                      </p>
+                    </div>
+                  </div>
+                  <button
+                    name="Add to cart"
+                    className="product__btn"
+                    onClick={handleAddToCart}
+                    style={{
+                      background: "black",
+                      color: "white",
+                      fontSize: "0.75rem",
+                      textTransform: "uppercase",
+                      padding: "0.5rem 1rem",
+                      transition: "all 0.3s ease-in-out",
+                      marginTop: "1rem",
+                      border: "none",
+                      marginBottom: "0.5rem",
+                      marginTop: "0.5rem",
+                    }}
+                  >
+                    Quick add
+                  </button>
+                </div>
+              </container>
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
     </div>
   );
 };
