2ca9cedf43c487ae9c3207b72a705b4656723613
Marvin <marvinthompson@pursuit.org>
2022-11-19 00:23:43 -0500

    Added footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyles";
+
+const Footer = () => {
+  return (
+    <Box>
+      <Container>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <FooterLink href="#">TRASHPLXNET</FooterLink>
+            <FooterLink href="#">[NULL]</FooterLink>
+            <FooterLink href="#">Vision</FooterLink>
+            <FooterLink href="#">Contact us</FooterLink>
+          </Column>
+
+          <Column>
+            <Heading>Community</Heading>
+            <FooterLink href="#">
+              <i className="fab fa-facebook-f">
+                <span style={{ marginLeft: "10px" }}>Facebook</span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-instagram">
+                <span style={{ marginLeft: "10px" }}>Instagram</span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-twitter">
+                <span style={{ marginLeft: "10px" }}>Twitter</span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-youtube">
+                <span style={{ marginLeft: "10px" }}>Youtube</span>
+              </i>
+            </FooterLink>
+          </Column>
+        </Row>
+      </Container>
+    </Box>
+  );
+};
+export default Footer;
